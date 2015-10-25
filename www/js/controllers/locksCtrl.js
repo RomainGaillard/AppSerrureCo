c4e010b7799a806b0c6e6b395f7baad6c766610e
RomainGaillard <romain.gaillard@y-nov.com>
2015-10-26 00:23:02 +0100

    Ajout de la popup askGroup et de la popup pour rejoindre un groupe.

--- www/js/controllers/locksCtrl.js
+++ www/js/controllers/locksCtrl.js
@@ -23,7 +23,21 @@ angular.module("locks.controllers")
 
         // Create and load the Modal
         $ionicModal.fromTemplateUrl('templates/new_group.html', function(modal) {
-            $scope.taskModal = modal;
+            $scope.newGroupModal = modal;
+        }, {
+            scope: $scope,
+            animation: 'slide-in-up'
+        });
+
+        $ionicModal.fromTemplateUrl('templates/ask_group.html', function(modal) {
+            $scope.askGroupModal = modal;
+        }, {
+            scope: $scope,
+            animation: 'slide-in-up'
+        });
+
+        $ionicModal.fromTemplateUrl('templates/join_group.html', function(modal) {
+            $scope.joinGroupModal = modal;
         }, {
             scope: $scope,
             animation: 'slide-in-up'
@@ -31,20 +45,44 @@ angular.module("locks.controllers")
 
         // Open our new group modal
         $scope.newGroup = function(){
-            $scope.taskModal.show();
+            $scope.newGroupModal.show();
+            $scope.closeAskGroup();
         }
 
+        $scope.askGroup = function(){
+            $scope.askGroupModal.show();
+        }
+
+        $scope.joinGroup = function(){
+            $scope.joinGroupModal.show();
+            $scope.closeAskGroup();
+        }
         // Called when the form is submitted
         $scope.createGroup = function(task) {
             //TodolistService.addItem(task.title);
-            $scope.taskModal.hide();
+            $scope.newGroupModal.hide();
+            task.title = "";
+            //$scope.todolist = TodolistService.getTodolist();
+        };
+
+        $scope.requestJoinGroup = function(task) {
+            //TodolistService.addItem(task.title);
+            $scope.joinGroupModal.hide();
             task.title = "";
             //$scope.todolist = TodolistService.getTodolist();
         };
 
         // Close the new group modal
         $scope.closeNewGroup = function() {
-            $scope.taskModal.hide();
+            $scope.newGroupModal.hide();
+        };
+
+        $scope.closeAskGroup = function() {
+            $scope.askGroupModal.hide();
+        };
+
+        $scope.closeJoinGroup = function() {
+            $scope.joinGroupModal.hide();
         };
 
     }])
