ddc45e2d4f243542b09b27486557eed1b7ad81ca
aquakiss <romain.menard17@gmail.com>
2015-11-05 11:48:43 +0100

    add modif constantsSrv.js

---
+++ www/js/services/constantsSrv.js
@@ -0,0 +1,11 @@
+/**
+ * Created by Romain Gaillard on 29/10/2015.
+ */
+
+angular.module('constants.services')
+
+    .service('ConstantsSrv', function() {
+        var domain = "http://192.168.0.44:1337/"
+        this.createGroup = domain+"group/create";
+        this.login = domain+"auth/local";
+    });
