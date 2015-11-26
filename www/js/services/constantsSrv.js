ffaf9ebc25d2275993a16cc0903ced877aa00690
Bastien <gbastien2@hotmail.fr>
2015-11-26 16:39:51 +0100

    modification de l'ip en localhost

--- www/js/services/constantsSrv.js
+++ www/js/services/constantsSrv.js
@@ -6,7 +6,8 @@ angular.module('constants.services')
 
     .service('ConstantsSrv', function() {
 
-        var domain = "http://10.33.0.52:1337/";
+        var domain = "http://localhost:1337/";
+        //var domain = "http://10.33.0.52:1337/";
 
         this.group           = domain+"group";
         this.createGroup     = domain+"group/create";
